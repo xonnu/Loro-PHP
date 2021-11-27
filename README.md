04794483892f66742ac27606465ab1d2594e1e2e
heychrono <chrono.social@outlook.com>
2021-11-28 01:31:42 +0800

    Update function added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Chrono CRUD
+```php
+$query = createInsertQuery($databaseConnection, "account", [
+    "name" => "Justin Pascual",
+    "email" =>  "example@gmail.com",
+    "password" => password_hash("pass123", PASSWORD_DEFAULT)
+]);
+
+$isInserted = createData($databaseConnection, $query);
+    
+if($isInserted) {
+    echo "data inserted";
+}
+```
--- chrono-crud.php
+++ chrono-crud.php
@@ -1,4 +1,7 @@
 <?php   
+    /*
+    CR    
+    */
     /* For sample use only */
     $databaseConnection = @mysqli_connect('localhost', 'root', '', 'test');
     
@@ -6,57 +9,66 @@
         return mysqli_close($databaseConnection);
     }
 
+    /**
+     * Execute queries created
+     * @param object $databaseConnection put your database connection here
+     * @param string $query write your SQL statement or query here 
+     */
+
+    function executeQuery(object $databaseConnection, string $query) : bool {
+        return mysqli_query($databaseConnection, $query);
+    }
+    
     function rowCount(object $databaseConnection, string $tableName) : int {
         return mysqli_num_rows(mysqli_query($databaseConnection, "SELECT * FROM $tableName"));
     }
 
+    function sanitize(object $databaseConnection, string $data) : string {
+        return @mysqli_real_escape_string($databaseConnection, htmlentities($data, ENT_COMPAT, 'UTF-8'));
+    }
+
     function sanitizeArray(object $databaseConnection, array $arrayOfData = []) : array {
 
-        foreach ($arrayOfData as $columnName => $columnValue) {
-            $columnName  = @mysqli_real_escape_string($databaseConnection, htmlentities($columnName, ENT_QUOTES, 'UTF-8'));
-            $columnValue = @mysqli_real_escape_string($databaseConnection, htmlentities($columnValue, ENT_QUOTES, 'UTF-8'));
-            $arrayOfData[$columnName] = $columnValue;
+        foreach ($arrayOfData as $columnKey => $columnValue) {
+            $columnKey   = @mysqli_real_escape_string($databaseConnection, htmlentities($columnKey, ENT_COMPAT, 'UTF-8'));
+            $columnValue = @mysqli_real_escape_string($databaseConnection, htmlentities($columnValue, ENT_COMPAT, 'UTF-8'));
+            $arrayOfData[$columnKey] = $columnValue;
         }
 
         return $arrayOfData;
     }
 
-    function sanitize(object $databaseConnection, string $data) : string {
-        return @mysqli_real_escape_string($databaseConnection, htmlentities($data, ENT_QUOTES, 'UTF-8'));
-    }
-
-    // Create Function
-    function createInsertQuery(object $databaseConnection, string $tableName, array $arrayOfData) : string {
+    function createInsertStatement(object $databaseConnection, string $tableName, array $arrayOfData) : string {
         $cleanArrayOfData = sanitizeArray($databaseConnection, $arrayOfData);
         $columnNames = implode(', ', array_keys($cleanArrayOfData));
         $columnValues = implode("', '", array_values($cleanArrayOfData));
-
         return "INSERT INTO $tableName($columnNames) VALUES('$columnValues')";
     }
 
-    function createData(object $databaseConnection, string $query) : bool {
-        return mysqli_query($databaseConnection, $query);
-    }
-
-    // Read Function
-    function createReadAllQuery(string $tableName, $selectColumn = '*', array $rowOption = [0, 0]) : string {
+    // Read Query Function
+    function createReadAllQuery(string $tableName, $selectColumn = '*', array $where = [], array $rowOption = [0, 0]) : string {
         if($selectColumn == '*') {
             $selectColumn = '*';
-        } else {
-            if(is_array($selectColumn)) {
-                $selectColumn = implode(', ', array_values($selectColumn));
-            }
+        } else if(is_array($selectColumn)) {
+            $selectColumn = implode(', ', array_values($selectColumn));
         }
 
         $isRowOptionChanged = $rowOption[0] != 0 && $rowOption[1] != 0; 
-
         if($isRowOptionChanged) {
             return "SELECT $selectColumn FROM $tableName LIMIT {$rowOption[0]}, {$rowOption[1]}";
+        }
+
+        if(count($where) != 0 && count($where) == 1) {
+            $tableColumn = key($where);
+            return "SELECT $selectColumn FROM $tableName WHERE $tableColumn= $where[$tableColumn]";
+        } else {
+            return "Error: Please use only one key and one value";
         }
 
         return "SELECT $selectColumn FROM $tableName";
     }
 
+    // Does not use @executeQuery Function
     function readAllData(object $databaseConnection, string $query, string $returnType = 'ARRAY') {
         $result = mysqli_query($databaseConnection, $query);
 
@@ -87,22 +99,78 @@
     }
 
     function createReadQuery(string $tableName, string $columnName, array $where = []) : string {
-        if(count($where) == 0) {
+        $whereArrayCount = count($where);
+        if($whereArrayCount == 0) {
             return "SELECT $columnName FROM $tableName";
+        }
+
+        if($whereArrayCount != 1) {
+            return "Error: Please use only one key and one value";
         }
 
         $tableColumn = key($where);
         $columnValue = $where[$tableColumn];
         
-        return "SELECT $columnName FROM $tableName WHERE $tableColumn = $columnValue";
+        return "SELECT $columnName FROM $tableName WHERE $tableColumn = '$columnValue'";
     }
 
     function readData(object $databaseConnection, string $query) {
-        return mysqli_fetch_row(mysqli_query($databaseConnection, $query))[0];
+        return @mysqli_fetch_row(mysqli_query($databaseConnection, $query))[0];
     }
 
-    // Example here
-    echo readData($databaseConnection, createReadQuery('account', 'name', ['id' => '8']));
+    function createUpdateStatement(string $tableName, $set, array $where = []) : string {
+        $whereArrayCount = count($where);
+        
+        if($whereArrayCount == 0) {
+            die("Error: WHERE cannot be empty");
+            return "";
+        }
+
+        if($whereArrayCount != 1) {
+            die("Error: Please use only one key and one value");
+            return "";
+        }
+
+        $newSet = [];
+        foreach ($set as $key => $value) {
+           $newSet[] = "$key = '$value'";
+        }
+
+        $setData = implode(', ', $newSet);
+        $tableColumn = key($where);
+
+        return "UPDATE $tableName SET $setData WHERE $tableColumn = $where[$tableColumn]";
+    }
+
+    $tableName = 'account';
+
     
+    $userEmail = 'justin.developer@gmail.com';
+    $getEmailQuery = createReadQuery('account', 'email', ['email' => $userEmail]);
+    $getEmail = readData($databaseConnection, $getEmailQuery);
+    $isAlreadyRegistered = ($userEmail == $getEmail);
+    
+    if(!$isAlreadyRegistered) {
+       $insertStatement = createInsertStatement($databaseConnection, $tableName,
+        [
+            'name' => 'Justin Pascual',
+            'email' => $userEmail,
+            'password' => password_hash("hey123", PASSWORD_DEFAULT)
+        ]);
+
+        $isExecuted = executeQuery($databaseConnection, $insertStatement);
+
+        if(!$isExecuted) {
+            echo "not executed";
+        }
+
+        echo "data inserted";
+    } else {
+        echo 'already registered';
+    }
+
+    
+
+
     closeConnection($databaseConnection);
 ?>
