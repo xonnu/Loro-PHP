cffb931d29dd658a0400108e9fccde90546818c2
Kurono <chrono.social@outlook.com>
2022-12-09 23:22:40 +0800

    refactor: Read single data example

--- read_data.php
+++ read_data.php
@@ -8,12 +8,6 @@ $database_connection = new mysqli('localhost', 'root', '', 'chrono-crud-example'
 $loro = new Loro($database_connection);
 $loro->tableName('account');
 
+// Print single data from database.
 $read_statement = $loro->readQuery('username', ['user_id' => 'LORO_386639']);
-$read_statement->execute();
-$is_executed = $read_statement->execute();
-
-if ($is_executed) {
-    print "Executed";
-} else {
-    print "Failed";
-}
+print $read_statement->read();
